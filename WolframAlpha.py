df05f0f344bac04db49c48a550469de8b847666f
vinnyasa <vinnyasa@gmail.com>
2018-01-05 07:30:11 -0800

    Added integration to Wikipedia (parser could be better). Also, cleaned Wolfram.py

--- WolframAlpha.py
+++ WolframAlpha.py
@@ -4,15 +4,16 @@
 
 import wolframalpha
 
-    #promp user for a question
+def  fetchWolfram():
+    # promp user for a question
     input = raw_input("How can I help? ")
 
-    #request from api
-    client = wolframalpha.Client("W7R5JE-T5TL7U6P8V") #app_id can not be here, we need to hide this.
+    # request to api
+    client = wolframalpha.Client("W7R5JE-T5TL7U6P8V")  # hide this.
 
     response = client.query(input)
 
-    #show answer
+    # show answer
     print next(response.results).text
 
-
+fetchWolfram()
