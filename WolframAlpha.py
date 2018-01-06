90bb079377f01080b5d7e4d0eecff88d6e743250
vinnyasa <vinnyasa@gmail.com>
2018-01-06 15:12:57 -0800

    Added BeaverGUI

--- WolframAlpha.py
+++ WolframAlpha.py
@@ -4,16 +4,17 @@
 
 import wolframalpha
 
-def  fetchWolfram():
-    # promp user for a question
-    input = raw_input("How can I help? ")
 
+def fetchWolfram(input):
     # request to api
     client = wolframalpha.Client("W7R5JE-T5TL7U6P8V")  # hide this.
 
     response = client.query(input)
 
     # show answer
-    print next(response.results).text
+    print(next(response.results).text)
 
-#fetchWolfram()
+
+
+param = input("How can I help? ")
+fetchWolfram(param)
